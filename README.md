dfc02e53dfad1e54065fdea2b40894c180dddd88
Thomas Hodges <me@thodges.com>
2018-12-27 16:34:36 -0600

    Update to easier logic and README

--- README.md
+++ README.md
@@ -8,14 +8,35 @@ External adapter for use on Google Cloud Platform or AWS Lambda. Zip and upload,
 npm install
 ```
 
-Create zip:
+## Test
+
+```bash
+npm test
+```
+
+## Create zip
 
 ```bash
 zip -r cl-cc.zip .
 ```
 
-## Test
+## Install to AWS Lambda
 
-```bash
-mocha
-```
+- In Lambda Functions, create function
+- On the Create function page:
+  - Give the function a name
+  - Use Node.js 8.10 for the runtime
+  - Choose an existing role or create a new one
+  - Click Create Function
+- Under Function code, select "Upload a .zip file" from the Code entry type drop-down
+- Click Upload and select the `cl-cc.zip` file
+- Handler should remain index.handler
+- Save
+
+
+## Install to GCP
+
+- In Functions, create a new function, choose to ZIP upload
+- Click Browse and select the `cl-cc.zip` file
+- Select a Storage Bucket to keep the zip in
+- Function to execute: gcpservice
--- index.js
+++ index.js
@@ -4,43 +4,21 @@ const createRequest = (input, callback) => {
     let url = "https://min-api.cryptocompare.com/data/";
     const endpoint = input.data.endpoint || "";
     url = url + endpoint;
-    const fsyms = input.data.fsyms || "";
-    const fsym = input.data.fsym || "";
-    const tsyms = input.data.tsyms || "";
-    const tsym = input.data.tsym || "";
-    const exchange = input.data.exchange || "";
-    let requestObj;
-    switch (endpoint) {
-        case "price":
-            requestObj = {
-                fsym: fsym,
-                tsyms: tsyms
-            }
-            break;
-        case "pricemulti":
-        case "pricemultifull":
-            requestObj = {
-                fsyms: fsyms,
-                tsyms: tsyms
-            }
-            break;
-        case "generateAvg":
-            requestObj = {
-                fsym: fsym,
-                tsym: tsym,
-                e: exchange
-            }
-            break;
-        default:
-            requestObj = {
-                fsym: fsym,
-                tsyms: tsyms
-            }
-            break;
+    let queryObj = {
+        fsym: input.data.fsym,
+        fsyms: input.data.fsyms,
+        tsym: input.data.tsym,
+        tsyms: input.data.tsyms,
+        exchange: input.data.exchange
     }
-    let options = {
+    for (let key in queryObj) {
+        if (typeof queryObj[key] === "undefined") {
+            delete queryObj[key];
+        }
+    }
+    const options = {
         url: url,
-        qs: requestObj,
+        qs: queryObj,
         json: true
     }
     request(options, (error, response, body) => {
